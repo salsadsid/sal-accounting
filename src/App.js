a70f7c8e1f7053f9222a6bab1cbd14a6e5c27876
salsadsid <salman.dnj@gmail.com>
2022-04-18 02:51:39 +0600

    login-register-toggle-activated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Checkout from './Pages/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -15,6 +16,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -2,10 +2,10 @@ import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('')
-
+    const nameRef = useRef('')
     const handleLogin = event => {
         event.preventDefault()
         const email = emailRef.current.value
@@ -14,8 +14,13 @@ const Login = () => {
     }
     return (
         <div className='w-50 mx-auto'>
-            <h2 className='text-center text-dark mt-3'>Please Login</h2>
+            <h2 className='text-center text-dark mt-3'>Please Register</h2>
             <Form onSubmit={handleLogin} className='w-100'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control ref={nameRef} type="text" placeholder="Your Name" />
+
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
 
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
@@ -28,12 +33,12 @@ const Login = () => {
                 </Form.Group>
 
                 <Button className='w-50 mx-auto d-block' variant="dark" type="submit">
-                    Login
+                    Register
                 </Button>
-                <p className='mt-3'>New To Sal Accounting? <Link to='/register' className='text-success text-decoration-none' >Please Register!</Link></p>
+                <p className='mt-3'>Already an account? <Link to='/login' className='text-success text-decoration-none' >Please Login!</Link></p>
             </Form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
